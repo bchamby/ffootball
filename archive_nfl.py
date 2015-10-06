298fce00ac6f3c09a51cb5bfad246607a97e2fb3
Brad Chamberlain <chamby@gmail.com>
2015-10-06 14:55:06 -0400

    initial commit

---
+++ archive_nfl.py
@@ -0,0 +1,21 @@
+########################
+# Author: Brad Chamberlain
+# Summary: Moves NFL fantasy data to archive folder.
+########################
+
+import time
+
+files = [
+'nfl_dst.html'
+'nfl_k.html'
+'nfl_qb.html'
+'nfl_rb.html'
+'nfl_te.html'
+'nfl_wr.html'
+
+CurrentDate = time.strftime("%Y-%m-%d")
+
+for file in files:
+    shutil.move("./output" + file, "./output" + file + currentTime)
+
+#print (time.strftime("%Y-%m-%d"))
